7bd99a0e5047eecb8647214c0eacf836494f2255
unknown <briannie254@gmail.com>
2023-02-03 15:12:01 +0300

    update server code

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.post("/work", function(req, res) {
   res.redirect("/work");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server running on port 3000.");
 })
